c40a40a7253ae1896f6c3ca39f361d80ea91a7bb
woojin sang <tkdalsrb147@naver.com>
2023-07-10 10:17:47 +0900

    불필요한 코드 제거

--- 07/0707_excel_기반_영문_텍스트_수정/main1.py
+++ 07/0707_excel_기반_영문_텍스트_수정/main1.py
@@ -17,7 +17,6 @@ for idx in tqdm(range(input1.shape[0])):
     property_group = group[property].tolist()   # input1에 속성값인 group에 key값의 단어들을 리스트 형태로 불러옴
     
     df = input1.loc[idx].copy()
-    sample_df = input1.loc[idx].copy()
     lower_sentence = input1.loc[idx]['문장'].lower()
     sample = input1.loc[idx]['문장']
     
@@ -25,45 +24,14 @@ for idx in tqdm(range(input1.shape[0])):
     for i in property_group:
         word = i.lower()
         if word in lower_sentence:    # 단어가 해당 문장에 있을 경우
-            if sample == df['문장']:    # 매칭된 단어가 없어 변경이 되지 않았을 경우
-                if lower_sentence.count(word) == 1:    # 문장에 단어가 한개일 경우
-                    index = -1
-                    while True:     # 한 개 이상일 경우 중복되지 않게 한 문장을 여러 문장으로 전처리하여 생성
-                        index = lower_sentence.find(word, index+1)     # 단어의 인덱스를 찾는다. 찾는 문자가 존재하면 해당 위치의 index를 반환, 존재하지 않다면 -1을 반환
-                        if index == -1: 
-                            break
-                        df['문장'] = sample[:index] + '$' + sample[index:index+len(i)] + '$' + sample[index+len(i):]   # 문장의 단어를 전처리하여 저장 
-                        df2 = df.copy()
-                        df_raw_list.append(df2)
-                else:
-                    index = -1
-                    while True:     
-                        index = lower_sentence.find(word, index+1) 
-                        if index == -1: 
-                            break
-                        df['문장'] = sample[:index] + '$' + sample[index:index+len(i)] + '$' + sample[index+len(i):] 
-                        df2 = df.copy()
-                        df_raw_list.append(df2)
-                        
-            else:   # 매칭된 단어가 존재해 변경이 되었을 경우
-                if lower_sentence.count(word) == 1:
-                    index = -1
-                    while True:
-                        index = lower_sentence.find(word, index+1)
-                        if index == -1:
-                            break
-                        sample_df['문장'] = sample[:index] +  '$' + sample[index:index+len(i)] + '$' + sample[index+len(i):]
-                        sample_df2 = sample_df.copy()
-                        df_raw_list.append(sample_df2)
-                else:
-                    index = -1
-                    while True:
-                        index = lower_sentence.find(word, index+1)
-                        if index == -1:
-                            break
-                        sample_df['문장'] = sample[:index] +  '$' + sample[index:index+len(i)] + '$' + sample[index+len(i):]
-                        sample_df2 = sample_df.copy()
-                        df_raw_list.append(sample_df2)
+            index = -1
+            while True:     # 한 개 이상일 경우 중복되지 않게 한 문장을 여러 문장으로 전처리하여 생성
+                index = lower_sentence.find(word, index+1)     # 단어의 인덱스를 찾는다. 찾는 문자가 존재하면 해당 위치의 index를 반환, 존재하지 않다면 -1을 반환
+                if index == -1: 
+                    break
+                df['문장'] = sample[:index] + '$' + sample[index:index+len(i)] + '$' + sample[index+len(i):]   # 문장의 단어를 전처리하여 저장 
+                df2 = df.copy()
+                df_raw_list.append(df2)
 
     # 문장에 매칭 단어가 없을 경우
     if input1.loc[idx]['문장'] == df['문장']:
@@ -75,6 +43,6 @@ raw_df = pd.DataFrame(df_raw_list)
 raw_df = raw_df.sort_values('key값')
 
 
-raw_df.to_excel(f'{save_dir}/new.xlsx', index=False)
+raw_df.to_excel(f'{save_dir}/new1.xlsx', index=False)
 
 
