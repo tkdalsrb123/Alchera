a786f4c43b58f3b5b75164a35cc6c305ead57aa9
woojin sang <tkdalsrb147@naver.com>
2023-07-12 16:35:22 +0900

    우선순위 변경으로 일단 중지

---
+++ 07/0712_extract_s3/config.py
@@ -0,0 +1,5 @@
+access_key = '566CE6CA2BAC1531C03C'
+secret_key = '9E44751BE123A977E51ABB3B3F7829B6B34DFFA6'
+endpoint_url = 'https://kr.object.ncloudstorage.com'
+region_name = 'kr-standard'
+bucket_name = ''
