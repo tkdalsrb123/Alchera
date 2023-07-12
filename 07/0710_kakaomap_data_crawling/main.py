564c15594b0eeaa53289516ed3ac920e3c7ae855
woojin sang <tkdalsrb147@naver.com>
2023-07-12 15:42:30 +0900

    셀레니움 연습

--- 07/0710_kakaomap_data_crawling/main.py
+++ 07/0710_kakaomap_data_crawling/main.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.mouse_button import MouseButton
 
-text = '전북 장수군 장수읍 개실길'
+text = '전북 장수군 장수읍 개실길\n'
 
 driver = webdriver.Chrome()
-url = 'https://search.map.kakao.com/mapsearch/map.daum?callback=jQuery18109267327558913874_1689065809524&q=+%EC%A0%84%EB%B6%81+%EC%9E%A5%EC%88%98%EA%B5%B0+%EC%9E%A5%EC%88%98%EC%9D%8D+%EA%B0%9C%EC%8B%A4%EA%B8%B8&msFlag=A&sort=0'
+url = 'https://map.kakao.com/'
 driver.get(url)
+driver.implicitly_wait(time_to_wait=3)  # 웹페이지 load 최대시간 설정
+
+q_element = driver.find_element(By.NAME, 'q')     # 검색창 element 찾기
+q_element.send_keys(text, Keys.ENTER)   # 검색창에 text 넣기
+clickable = driver.find_element(By.ID, 'search.keyword.submit')
+ActionChains(driver)\
+    .context_click(clickable)\
+    .perform()
+driver.save_screenshot('./image.png')
+driver.quit()
 
