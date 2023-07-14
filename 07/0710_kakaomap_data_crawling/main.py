d6745d6a01f2beb0a8920d58a11d8ba24d9610db
woojin sang <tkdalsrb147@naver.com>
2023-07-14 17:40:07 +0900

    로드뷰 크롤링 코드 제작

--- 07/0710_kakaomap_data_crawling/main.py
+++ 07/0710_kakaomap_data_crawling/main.py
@@ -23,7 +23,7 @@ from selenium.webdriver.support import expected_conditions as EC
 #     wait.until(EC.element_to_be_clickable((By.XPATH, '//div[contains(@id,"_ar_10_")]')))
 # finally:
 #     driver.quit()
-text = '전라북도 장수군 장수읍 개정농원길'
+text = '전라북도 장수군 장수읍 개실길'
 
 driver = webdriver.Chrome()
 url = f'https://map.kakao.com/?q={text}'
@@ -37,8 +37,24 @@ point = driver.find_elements(By.XPATH, '//*[contains(@style, "position: absolute
 actions = ActionChains(driver)
 actions.double_click(point[-1])
 actions.perform()
-time.sleep(5)
-wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="view.mapContainer"]/div[2]/div/div[6]/*')))
+time.sleep(3)
+button = driver.find_element(By.XPATH, '//*[@id="view"]/div[1]/div[1]/button')
+screenshot = driver.find_element(By.XPATH, '//*[@id="view"]/div[1]/div[1]/ul/li[1]/button')
+canvas = driver.find_element(By.XPATH, '//*[@id="view.roadview"]')
+clickbutton = ActionChains(driver)\
+    .move_to_element(canvas)\
+    .click_and_hold()\
+    .move_by_offset(400,0)\
+    .release()\
+    .click(button)
+clickbutton.perform()
+for i in range(3):
+    time.sleep(5)
+    clickscreenshot = ActionChains(driver)\
+        .click(screenshot)\
+        .move_by_offset(-100, 50)\
+        .click()
+    clickscreenshot.perform()
+time.sleep(3)
+wait.until(EC.element_to_be_clickable((By.XPATH, '//*[contains(@style, "position: absolute; z-index: 3;")]')))
 driver.quit()
-# //*[@id="view.mapContainer"]/div[2]/div/div[6]/div[9]
-# //*[@id="view.mapContainer"]/div[2]/div/div[6]/div[6]
