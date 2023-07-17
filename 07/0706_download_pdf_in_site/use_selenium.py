2b032990d5925d1389dc16a790b4844a31a89f3f
woojin sang <tkdalsrb147@naver.com>
2023-07-17 14:31:24 +0900

    크롤링 연습

--- 07/0706_download_pdf_in_site/use_selenium.py
+++ 07/0706_download_pdf_in_site/use_selenium.py
@@ -10,32 +10,35 @@ from bs4 import BeautifulSoup
 url_title = {'시황정보 리포트':'company_list.naver', '투자정보 리포트':'invest_list.naver', '종목분석 리포트':'company_list.naver', '산업분석 리포트':'industry_list.naver', '경제분석 리포트':'economy_list.naver', '채권분석 리포트':'debenture_list.naver'}
 
 excel = pd.read_excel(r"C:\Users\Alchera115\wj.alchera\Alchera_data\07\0706_사이트 내 PDF 파일 다운로드\save_pdf.xlsx")
+# excel['날짜'] = excel['날짜'].str.replace('.', '/')
+excel['날짜'] = pd.to_datetime(excel['날짜'], format='%y.%m.%d')
 data = excel.values
+print(data)
+# driver = webdriver.Chrome()
+# pdf_list = []
+# date_list = []
 
-driver = webdriver.Chrome()
-pdf_list = []
-date_list = []
-
-for val in data[:3]:
-    title = url_title[val[0]]
-    date = val[1]
-    url = f'https://finance.naver.com/research/{title}'
-    driver.get(url)
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-    table = soup.find('table')
+# for val in data[:3]:
+#     title = url_title[val[0]]
+#     date = val[1]
+#     url = f'https://finance.naver.com/research/{title}'
+#     driver.get(url)
     
-    # 작성일 추출
-    for date in table.find_all('td', {'style':'padding-left:5px'}):
-        date_list.append(date.text)
+#     html = driver.page_source
+#     soup = BeautifulSoup(html, 'html.parser')
+#     table = soup.find('table')
+    
+#     # 작성일 추출
+#     for date in table.find_all('td', {'style':'padding-left:5px'}):
+#         date_list.append(date.text)
         
-    # pdf 링크 추출
-    for pdf_link in table.find_all('td', {'class':'file'}):
-        if pdf_link.find('a') != None:
-            pdf_list.append(pdf_link.find('a').get('href'))
-            # print(pdf_link.find('a').get('href'))
+#     # pdf 링크 추출
+#     for pdf_link in table.find_all('td', {'class':'file'}):
+#         if pdf_link.find('a') != None:
+#             pdf_list.append(pdf_link.find('a').get('href'))
+#             # print(pdf_link.find('a').get('href'))
     
-    date_pdf = list(zip(date_list, pdf_list))
+#     date_pdf = list(zip(date_list, pdf_list))
             
 
     
