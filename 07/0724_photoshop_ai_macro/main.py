efc5b17cf0068274c66aee342062564e19dbd8da
woojin sang <tkdalsrb147@naver.com>
2023-07-26 15:28:23 +0900

    사진 포토샵 매크로

--- 07/0724_photoshop_ai_macro/main.py
+++ 07/0724_photoshop_ai_macro/main.py
@@ -3,34 +3,48 @@ from pywinauto import findwindows
 from pywinauto.controls.common_controls import ToolbarWrapper
 import time
 from pywinauto import mouse
+from pywinauto.keyboard import SendKeys, KeySequenceError
 import win32api
+from pywinauto.controls.menuwrapper import MenuItem
+from pywinauto.controls.hwndwrapper import HwndWrapper
 
 # procs = findwindows.find_elements()
 
 # for proc in procs:
 #     print(proc, proc.process_id)
 
-# x, y = win32api.GetCursorPos()
-app = Application(backend="uia").connect(path = r"C:\Program Files\Adobe\Adobe Photoshop (Beta)\Photoshop.exe")
+app = Application(backend="uia").connect(path=r"C:\Program Files\Adobe\Adobe Photoshop (Beta)\Photoshop.exe")
 
 
+# mouse control
 dlg = app['Dialog']
-dlg['최대화'].click()
-mouse.click(button='left', coords=(80, 200))
+# dlg['최대화'].click()
+mouse.click(button='left', coords=(52, 13))
 time.sleep(1)
-mouse.double_click(button='left', coords=(250, 200))
+mouse.click(button='left', coords=(60, 50))
 time.sleep(1)
-# app.dlg.print_control_identifiers()
-# print(app.dlg.print_control_identifiers())
-
-# app.Dialog.menu_select('파일(F)')
-# app.Dialog.파일(F).Open.Edit.SetEditText(r"C:\Users\Alchera115\wj.alchera\Alchera_data\07\0724_photoshop_ai_macro\축사피해-로드뷰\축사피해-로드뷰\로드뷰_경남 고성군 개천면 - 2023-07-18T194546.145.png")
-# app['Dialog']['메뉴 모음'].print_control_identifiers()
-# dlg['메뉴 모음']['파일(F)'].child_window(title='파일(F)', control_type="MenuItem").select()
-# dlg.print_control_identifiers()
-# time.sleep(1)
-# ToolbarWrapper.menu_bar_click_input(menu)
-# app.dlg.child_window(title='메뉴 모음', control_type='MenuBar')
-# dlg.print_control_identifiers()
-# dlg['메뉴 모음']['파일(F)'].click()
-
+mouse.double_click(button='left', coords=(230, 220))
+time.sleep(1)
+mouse.press(button='left', coords=(169, 265))
+time.sleep(2)
+mouse.move(coords=(275, 330))
+time.sleep(1)
+mouse.release(button='left', coords=(275, 330))
+time.sleep(1)
+for i in range(130):
+    SendKeys('+{RIGHT}')
+for i in range(60):
+    SendKeys('+{DOWN}')
+time.sleep(1)
+mouse.click(button='left', coords=(910, 754))
+time.sleep(1)
+app['Dialog']['Pane'].type_keys("korea wild boar{ENTER}", with_spaces=True)
+time.sleep(30)
+mouse.click(button='left', coords=(52, 13))
+time.sleep(1)
+mouse.click(button='left', coords=(125, 328))
+time.sleep(1)
+mouse.click(button='left', coords=(345, 360))
+time.sleep(1)
+SendKeys('{ENTER}')
+# app['Dialog'].print_control_identifiers()
