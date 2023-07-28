6693cb225beed7584362bad49c96a91b53b34e52
woojin sang <tkdalsrb147@naver.com>
2023-07-28 17:38:34 +0900

    keypoint 수정

--- 07/0727_keypoint_and_classname_vis/167_keypoint_vis.py
+++ 07/0727_keypoint_and_classname_vis/167_keypoint_vis.py
@@ -12,7 +12,27 @@ def read_img(img_path):
     img_array = np.fromfile(img_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
     return img
-        
+
+def visualization(image, keypoints, bbox, color):
+    # keypoints 시각화
+    for idx in range(0, len(keypoints), 3):
+        x = keypoints[idx]
+        y = keypoints[idx+1]
+        cv2.circle(image, (x,y), 5, color=color, thickness=-1)
+
+    # bbox 및 텍스트 시각화
+    x1 = bbox[0]
+    y1 = bbox[1]
+    x2 = x1 + bbox[2]
+    y2 = y1 + bbox[3]
+    cv2.rectangle(img, (x1,y1), (x2,y2), color=color, thickness=2)
+
+    if y1-10 > 1:
+        cv2.putText(image, text, (x1,y1-10), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=color)
+    else:   # 이미지 벗어날 경우 박스 하단에 시각화
+        cv2.putText(image, text, (x1,y2+15), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=color)
+
+
 _, input_dir, ouput_dir = sys.argv
 
 # 파일 수집
@@ -41,8 +61,6 @@ for path in matching_dict.values():
     action_name = json_file['annotationImageInfo']['action']
     
     img = read_img(image_path)
-    color = (0,0, 255)
-
     for obj in json_file['annotationObjectInfo']:
         action_value = obj['actionValue']
         coordinates_keypoints = obj['keypoints']
@@ -50,25 +68,17 @@ for path in matching_dict.values():
         
         text = f'{action_name}/{action_value}'
         
+        if action_value == True:
+            select_color = (0, 0, 255)
+        elif action_value == False:
+            select_color = (255, 0, 0)
+        
         # keypoints가 존재하는 obj info 시각화
         if len(coordinates_keypoints) > 0:
-            # keypoints 시각화
-            for idx in range(0, len(coordinates_keypoints), 3):
-                x = coordinates_keypoints[idx]
-                y = coordinates_keypoints[idx+1]
-                cv2.circle(img, (x,y), 5, color=color, thickness=-1)
+            visualization(img, coordinates_keypoints, coordinates_BBox, select_color)
 
-            # bbox 및 텍스트 시각화
-            x1 = coordinates_BBox[0]
-            y1 = coordinates_BBox[1]
-            x2 = x1 + coordinates_BBox[2]
-            y2 = y1 + coordinates_BBox[3]
-            cv2.rectangle(img, (x1,y1), (x2,y2), color=color, thickness=2)
-    
-            if y1-10 > 1:
-                cv2.putText(img, text, (x1,y1-10), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=color)
-            else:   # 이미지 벗어날 경우 박스 하단에 시각화
-                cv2.putText(img, text, (x1,y2+15), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=color)
+        else:
+            visualization(img, coordinates_keypoints, coordinates_BBox, select_color) 
                 
     mid = '\\'.join(root.split('\\')[len(input_dir.split('\\')):])
     folder = os.path.join(ouput_dir, mid)
