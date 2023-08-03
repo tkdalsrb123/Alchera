ed65c7cee82891943b0087f37aa3f31efbae0268
woojin sang <tkdalsrb147@naver.com>
2023-08-03 17:07:27 +0900

    s3 sampling 수정

--- 08/0803_s3_sampling/main.py
+++ 08/0803_s3_sampling/main.py
@@ -3,6 +3,15 @@ from config import access_key, secret_key, endpoint_url, region_name, bucket_nam
 import pandas as pd
 from tqdm import tqdm
 
+def down_file_path(output_dir, from_down_path):
+    mid, file = os.path.split(from_down_path)
+    folder = os.path.join(output_dir, mid)
+    os.makedirs(folder, exist_ok=True)
+    
+    output_file = os.path.join(folder, file)
+    print(output_file)
+    s3client.download_file(bucket_name, from_down_path, output_file)
+
 _, excel_dir, output_dir = sys.argv
 
 path_list = []
@@ -15,9 +24,15 @@ paginator = s3client.get_paginator('list_objects_v2')
 # down file
 for path in tqdm(path_list):
     mid, file = os.path.split(path)
-    folder = os.path.join(output_dir, mid)
-    os.makedirs(folder, exist_ok=True)
-    
-    output_file = os.path.join(folder, file)
-    print(output_file)
-    s3client.download_file(bucket_name, path, output_file)
+    # 확장자명이 있을 경우
+    if '' != os.path.splitext(file)[1]:
+        down_file_path(output_dir, path)
+    # 확장자명이 없을 경우
+    elif '' == os.path.splitext(file)[1]:
+        response = paginator.paginate(Bucket=bucket_name, Prefix=f'{mid}/', Delimiter='/')
+        for res in response:
+            for content in res['Contents'][1:]:
+                content_file = os.path.split(content['Key'])[-1]
+                name, ext = os.path.splitext(content_file)
+                if file == name:
+                    down_file_path(output_dir, content['Key'])
