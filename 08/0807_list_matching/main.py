3c529354ede9c5849b3eb9a849a3a846adc67299
woojin sang <tkdalsrb147@naver.com>
2023-08-07 18:09:08 +0900

    list_matching

--- 08/0807_list_matching/main.py
+++ 08/0807_list_matching/main.py
@@ -9,8 +9,8 @@ def set_file_name(video_title):
             if char.isalnum():
                 new_word.append(char)
         file_name.append(''.join(new_word))
+        
     file_name = ' '.join(file_name)
-    
     return file_name
 
 _, input_dir, csv_dir, output_dir = sys.argv
@@ -22,7 +22,7 @@ for csv_file in csv_list:
     if ext == '.csv':
         csv_path = os.path.join(csv_dir, csv_file)
         
-        csv_file = pd.read_csv(csv_path, encoding='utf-8')
+        csv_file = pd.read_csv(csv_path, encoding='utf-8-sig')
         
         csv_file['revise'] = csv_file['name'].apply(set_file_name)
         csv_file['match'] = csv_file['revise'].str.replace(' ', '')
@@ -33,13 +33,14 @@ output_df = pd.DataFrame(columns=['revise', 'duration', 'url', 'search_sum'])
 mp4_list = os.listdir(input_dir)
 for mp4_file in mp4_list:
     filename, ext = os.path.splitext(mp4_file)
-    filename = filename.replace(' ', '')
     if ext == '.mp4':
-        match_df = all_df.loc[all_df['match'] == filename, ['duration', 'url', 'search_sum', 'revise']]
-        if match_df.shape[0] == 2:
-            print(filename)
+        mp4_path = os.path.join(input_dir, mp4_file)
+        new_filename = filename.replace(' ', '')
+        new_mp4_path = os.path.join(input_dir, new_filename)
+        os.rename(mp4_path, new_mp4_path)
+        match_df = all_df.loc[all_df['match'] == new_filename, ['duration', 'url', 'search_sum', 'revise']]
         output_df = pd.concat([output_df, match_df], ignore_index=True)
-        
+
 output_df.drop_duplicates(subset='url', inplace=True)
 output_df.to_csv(f'{output_dir}/matcing_file.csv', encoding='utf-8-sig', index=False)
 # all_df.to_csv(f'{output_dir}/all_file.csv', encoding='utf-8-sig', index=False)
