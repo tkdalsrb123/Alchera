badcdf73bbe230f760ca26cbd2645f7ba1189d0a
woojin sang <tkdalsrb147@naver.com>
2023-08-09 17:21:14 +0900

    use_selenium 수정

--- 07/0706_download_pdf_in_site/use_selenium.py
+++ 07/0706_download_pdf_in_site/use_selenium.py
@@ -11,21 +11,19 @@ def down_pdf(driver):
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find('div', {'class':'box_type_m'})
+    board = table.find('table', {'class':'type_1'})
     td = table.find_all('td')
-
+    navi = table.find('table', {'class':'Nnavi'})
+    
     if category == 'company_list.naver' or category == 'industry_list.naver':
-        for tr in table.find_all('tr'):
+        for tr in board.find_all('tr'):
             if len(tr.find_all('td')) > 1:
                 for info in tr.find_all('td'):
-                    if ['on'] in info.attrs.values():   # 페이지 태그는 제외
-                        pass
-                    
-                    elif not info.attrs.values():
+                    if not info.attrs.values():
                         if info.find('a') != None:
                             title = info.text
                         else:
                             company = info.text
-                            
                     elif ['file'] in info.attrs.values():   # pdf 파일 다운
                         if info.find('a') != None:
                             pdf = info.find('a').get('href')
@@ -41,15 +39,11 @@ def down_pdf(driver):
                             pdf_name = 'No'
                 
                 all_list.append([val[0], title, company, pdf_name, date])
-                
     else:
         for tr in table.find_all('tr'):
             if len(tr.find_all('td')) > 1:
                 for info in tr.find_all('td'):
-                    if ['on'] in info.attrs.values():   # 페이지 태그는 제외
-                        pass
-                    
-                    elif 'padding-left:10px' in info.attrs.values() or 'padding-left:10' in info.attrs.values():
+                    if 'padding-left:10px' in info.attrs.values() or 'padding-left:10' in info.attrs.values():
                         title = info.text
                     
                     elif not info.attrs.values():
@@ -70,17 +64,75 @@ def down_pdf(driver):
                             pdf_name = 'No'
                 
                 all_list.append([val[0], title, company, pdf_name, date])
-                        
-    
-            
+
     # 다음 페이지가 있을 경우
     for i in td:
         if i.has_attr('class'):
             if 'pgRR' in i['class']:
-                next = driver.find_element(By.XPATH, '//*[@id="contentarea_left"]/div[3]/table[2]/tbody/tr/td[3]')
-                next.click()
-                down_pdf(driver)
-    
+                all_page = navi.find_all('a')
+                for page in all_page[1:-1]:
+                    next = page.get('href')
+                    url = f'https://finance.naver.com/{next}'
+                    
+                    driver.get(url)
+                    time.sleep(1)
+                    html = driver.page_source
+                    soup = BeautifulSoup(html, 'html.parser')
+                    table = soup.find('div', {'class':'box_type_m'})
+                    board = table.find('table', {'class':'type_1'})
+                    td = table.find_all('td')
+                    navi = table.find('table', {'class':'Nnavi'})
+                    
+                    if category == 'company_list.naver' or category == 'industry_list.naver':
+                        for tr in board.find_all('tr'):
+                            if len(tr.find_all('td')) > 1:
+                                for info in tr.find_all('td'):
+                                    if not info.attrs.values():
+                                        if info.find('a') != None:
+                                            title = info.text
+                                        else:
+                                            company = info.text
+                                    elif ['file'] in info.attrs.values():   # pdf 파일 다운
+                                        if info.find('a') != None:
+                                            pdf = info.find('a').get('href')
+                                            pdf_name = pdf.split('/')[-1]
+                                            response_pdf = requests.get(pdf)
+                                            down_path = os.path.join(folder, pdf_name)
+                                            pdf_file = open(down_path, 'wb')
+                                            pdf_file.write(response_pdf.content)
+                                            pdf_file.close()
+                                            print(down_path, 'downloaded')
+                                            
+                                        elif info.find('a') == None:    # pdf 파일이 없을 경우
+                                            pdf_name = 'No'
+                                
+                                all_list.append([val[0], title, company, pdf_name, date])
+                    else:
+                        for tr in table.find_all('tr'):
+                            if len(tr.find_all('td')) > 1:
+                                for info in tr.find_all('td'):
+                                    if 'padding-left:10px' in info.attrs.values() or 'padding-left:10' in info.attrs.values():
+                                        title = info.text
+                                    
+                                    elif not info.attrs.values():
+                                        company = info.text
+                                        
+                                    elif ['file'] in info.attrs.values():   # pdf 파일 다운
+                                        if info.find('a') != None:
+                                            pdf = info.find('a').get('href')
+                                            pdf_name = pdf.split('/')[-1]
+                                            response_pdf = requests.get(pdf)
+                                            down_path = os.path.join(folder, pdf_name)
+                                            pdf_file = open(down_path, 'wb')
+                                            pdf_file.write(response_pdf.content)
+                                            pdf_file.close()
+                                            print(down_path, 'downloaded')
+                                            
+                                        elif info.find('a') == None:    # pdf 파일이 없을 경우
+                                            pdf_name = 'No'
+                                
+                                all_list.append([val[0], title, company, pdf_name, date])
+                    
 _, excel_dir, save_dir = sys.argv
 
 url_category = {'시황정보 리포트':'market_info_list.naver', '투자정보 리포트':'invest_list.naver', '종목분석 리포트':'company_list.naver', '산업분석 리포트':'industry_list.naver', '경제분석 리포트':'economy_list.naver', '채권분석 리포트':'debenture_list.naver'}
---
+++ 08/0809_nara_site_data_crawling/main.py
@@ -0,0 +1,2 @@
+from selenium import webdriver
+https://www.g2b.go.kr/pt/menu/selectSubFrame.do?framesrc=https://www.g2b.go.kr:8340/search.do?category=TGONG&kwd=인공지능&currentPageNo=1
