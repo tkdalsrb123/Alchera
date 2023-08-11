e70925b969cb0f1face081ce555acc14511447b9
woojin sang <tkdalsrb147@naver.com>
2023-08-11 11:56:42 +0900

    나라장터 크롤링

--- 08/0809_nara_site_data_crawling/main.py
+++ 08/0809_nara_site_data_crawling/main.py
@@ -5,6 +5,8 @@ from datetime import timedelta
 from bs4 import BeautifulSoup
 import requests
 from urllib.request import urlopen
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 def request_url(cat):
     '''returns url for a  category'''
@@ -25,26 +27,31 @@ def request_url(cat):
 #     df = pd.read_html(cat_url)[0]
 #     df['search_term']=cat
     
-    return df
+    # return df
 # df = pd.read_csv(r"C:\Users\Alchera115\wj.alchera\Alchera_data\08\0809_nara_site_data_crawling\keyword.csv", encoding='cp949')
 # print(df)
 url = request_url('인공지능')
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-tbody = soup.find('tbody')
-tr = tbody.find_all('tr')
-div = tr[0].find_all('td' > 'div')
-for i in div:
-    # if ['class'] not in i.attrs.values():
-    if 'class':
-        print(i.text)
-    if i.name == 'a':
-        print(i.get('href'))
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+
+a = driver.find_elements(By.CLASS_NAME, "tl")
+for i in a:
+    print(i.get_attribute('outer_html'))
+    # if hasattr(att, 'href') == True:
+    #     print(att)
+
+# time.sleep(3)
+# response = requests.get(url)
+# soup = BeautifulSoup(response.text, 'html.parser')
+
+# tbody = soup.find('tbody')
+# tr = tbody.find_all('tr')
+# div = tr[0].find_all('td' > 'div')
+# for i in div:
+#     if 'class':
+#         print(i.text)
+#     if i.name == 'a':
+#         print(i.get('href'))
     
-# df = pd.read_html(url)[0]
-# print(df['공고명'])
-
-# response = urlopen(url)
-# soup = BeautifulSoup.get(response, 'html.parser')
-
-# print(soup)
