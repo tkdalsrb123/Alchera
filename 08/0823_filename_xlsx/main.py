996ff21af7b839fd633c6cf255ec6cb45a19ff6c
woojin sang <tkdalsrb147@naver.com>
2023-08-23 17:17:42 +0900

    파일명 엑셀로 정리

---
+++ 08/0823_filename_xlsx/main.py
@@ -0,0 +1,22 @@
+import os, sys
+import pandas as pd
+from tqdm import tqdm
+
+
+_, input_dir, output_dir = sys.argv
+
+
+df_list = []
+i = 1
+for root, dirs, files in os.walk(input_dir):
+    for file in files:
+        root1, filename_jpg = os.path.split(root)
+        root2, Type = os.path.split(root1)
+        filename_pdf = filename_jpg.split('_')[0]
+        
+        df_list.append([i , filename_pdf, filename_jpg, file, Type, root])
+        
+        i += 1
+        
+df = pd.DataFrame(df_list, columns=['No', 'filename_pdf', 'filename_jpg', 'crop_img_name', 'type', '저장 파일 경로'])
+df.to_excel(f'{output_dir}/file.xlsx', index=False)
---
+++ 08/0823_filename_xlsx/readme.txt
@@ -0,0 +1,7 @@
+2023-08-23
+
+1. 요청사항
+[저장파일 정리 xlsx 출력]
+
+2. 실행방법
+python main.py [input 경로] [output 경로]
