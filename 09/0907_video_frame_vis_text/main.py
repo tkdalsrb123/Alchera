ca7e8f3dd23a436f5e667702e9bc99f654a1d69b
woojin sang <tkdalsrb147@naver.com>
2023-09-08 10:21:25 +0900

    비디오 프레임 시각화

--- 09/0907_video_frame_vis_text/main.py
+++ 09/0907_video_frame_vis_text/main.py
@@ -1,5 +1,24 @@
 import cv2, json, sys, os
 from collections import defaultdict
+from PIL import Image, ImageDraw, ImageFont
+import logging
+from tqdm import tqdm
+
+def make_logger(log):
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    
+    # formatter
+    file_formatter = logging.Formatter("%(asctime)s [%(levelname)s:%(lineno)d] -- %(message)s")
+    # file_handler
+    file_handler = logging.FileHandler(log, mode='w')
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.INFO)
+    # logger.add
+    logger.addHandler(file_handler)
+    
+    return logger
+
 
 def readfiles(dir, Ext):
     file_dict= defaultdict(str)
@@ -14,12 +33,20 @@ def readfiles(dir, Ext):
 
 _, input_dir, output_dir = sys.argv
 
+logger = make_logger('log.log')
+
 mp4_dict = readfiles(input_dir, '.mp4')
 json_dict = readfiles(input_dir, '.json')
 
-for filename, mp4_path in mp4_dict:
+for filename, mp4_path in tqdm(mp4_dict.items()):
     json_path = json_dict[filename]
-
+    root, file = os.path.split(mp4_path)
+    mid = '\\'.join(root.split('\\')[len(input_dir.split('\\')):])
+    folder = os.path.join(output_dir, mid)
+    test_folder = os.path.join(folder, 'test')
+    os.makedirs(folder, exist_ok=True)
+    os.makedirs(test_folder, exist_ok=True)
+    
     with open(json_path, encoding='UTF-8') as f:
         json_file = json.load(f)
     
@@ -31,10 +58,73 @@ for filename, mp4_path in mp4_dict:
         
         video = cv2.VideoCapture(mp4_path)
         currentframe = 0
-        
+
         while True:
             ret, frame = video.read() 
             if not ret:
                 break
-                
+            
+            num = str(currentframe).zfill(8)
+            frame_filename = f'{filename}_{num}.jpg'
+            output_test_img_path = os.path.join(test_folder, frame_filename)
+            output_img_path = os.path.join(folder, frame_filename)
             if currentframe >= start_frame and currentframe <= end_frame:
+                x1 = 10
+                y1 = 10
+
+                font = cv2.FONT_HERSHEY_PLAIN
+            
+                overlay = frame.copy()
+                
+                fontScale = 3   # background 크기 조절(글씨 크기를 조절하면서 같이 조절해야함)
+                text_w, text_h = cv2.getTextSize(text, font, fontScale=fontScale, thickness=1)[0]
+                cv2.rectangle(overlay, (x1, y1), (x1+(text_w//5)*3+20, y1+text_h+20), (255,255,255), -1)
+                alpha = 0.5  # 배경 투명도 조절
+   
+                fontSize = 40   # 글씨 크기 조절
+                frame = cv2.addWeighted(frame, alpha, overlay, 1-alpha, 0)
+                
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)        
+                frame = Image.fromarray(frame)
+                fontpath = "malgunbd.ttf"
+                pil_font = ImageFont.truetype(fontpath, fontSize)
+                draw = ImageDraw.Draw(frame)
+
+                draw.text((x1+15, y1), text, font=pil_font, fill=(255, 0, 0) )
+
+                logger.info(f"{output_img_path} 저장!!")
+                frame.save(output_img_path, 'JPEG')
+                if currentframe <= start_frame+15 or currentframe >= end_frame-15:
+                    logger.info(f"{output_test_img_path} 저장!!")
+                    frame.save(output_test_img_path, 'JPEG')
+
+            elif currentframe < start_frame:
+                logger.info(f"{output_img_path} 저장!! 시각화 x")
+                result, encoded_img = cv2.imencode('.jpg', frame)
+                if result:
+                    with open(output_img_path, mode='w+b') as f:
+                        encoded_img.tofile(f)
+                if currentframe >= start_frame-15:
+                    logger.info(f"{output_test_img_path} 저장!! 시각화 x")
+                    result, encoded_img = cv2.imencode('.jpg', frame)
+                    if result:
+                        with open(output_test_img_path, mode='w+b') as f:
+                            encoded_img.tofile(f)
+                            
+            elif currentframe > end_frame:
+                logger.info(f"{output_img_path} 저장!! 시각화 x")
+                result, encoded_img = cv2.imencode('.jpg', frame)
+                if result:
+                    with open(output_img_path, mode='w+b') as f:
+                        encoded_img.tofile(f)
+                if currentframe <= end_frame+15:
+                    logger.info(f"{output_test_img_path} 저장!! 시각화 x")
+                    result, encoded_img = cv2.imencode('.jpg', frame)
+                    if result:
+                        with open(output_test_img_path, mode='w+b') as f:
+                            encoded_img.tofile(f)
+
+                
+            
+            
+            currentframe += 1
