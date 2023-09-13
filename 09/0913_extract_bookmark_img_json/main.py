76f3e45b5e04f191ac29bd0701cbf065c735f3a1
woojin sang <tkdalsrb147@naver.com>
2023-09-13 14:43:40 +0900

    꿀뷰

---
+++ 09/0913_extract_bookmark_img_json/main.py
@@ -0,0 +1,72 @@
+import os, sys, shutil
+from collections import defaultdict
+from tqdm import tqdm
+import logging
+
+def make_logger(log):
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    
+    # formatter
+    file_formatter = logging.Formatter("%(asctime)s [%(levelname)s:%(lineno)d] -- %(message)s")
+    # file_handler
+    file_handler = logging.FileHandler(log, mode='w')
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.INFO)
+    # logger.add
+    logger.addHandler(file_handler)
+    
+    return logger
+
+
+def readfiles(dir, Ext):
+    file_dict = defaultdict(str)
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            filename, ext = os.path.splitext(file)
+            if ext == Ext:
+                file_path = os.path.join(root, file)
+                
+                file_dict[filename] = file_path
+                
+    return file_dict
+
+def copy_file(input_path, ooutput_path):
+    logger.info(input_path)
+    root, file = os.path.split(input_path)
+    mid = '\\'.join(root.split('\\')[len(root_dir.split('\\')):])
+    folder = os.path.join(ooutput_path, mid)
+    os.makedirs(folder, exist_ok=True)
+    output_img_path = os.path.join(folder, file)
+    shutil.copy2(input_path, output_img_path)
+    logger.info(f'{output_img_path} 저장!!!')
+    
+_, bookmark_dir, root_dir, otuput_dir, file_type = sys.argv
+
+logger = make_logger('log.log')
+
+img_dict = readfiles(root_dir, '.jpg')
+json_dict = readfiles(root_dir, '.json')
+
+file_list = []
+
+with open(bookmark_dir, 'r', encoding='utf-16') as f:
+    bookmarks = f.readlines()
+    
+for bookmark in tqdm(bookmarks, desc='bookmark 읽는중'):
+    file = os.path.split(bookmark)[-1]
+    file = file.replace('\n', '')
+    filename, ext = os.path.splitext(file)
+    if ext == '.jpg' or ext == '.json':
+        file_list.append(filename)
+        
+if file_type == 'jpg':
+    for filename in tqdm(file_list, desc='파일복사중'):
+        img_path = img_dict.get(filename)
+        if img_path:
+            copy_file(img_path, otuput_dir)
+elif file_type == 'json':
+    for filename in tqdm(file_list, desc='파일복사중'):
+        json_path = json_dict.get(filename, otuput_dir)
+        if json_path:
+            copy_file(json_path)
