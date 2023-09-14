61227846bf2749f0ef5a7aa2d83e94059bea0400
woojin sang <tkdalsrb147@naver.com>
2023-09-14 14:12:58 +0900

    extract bookmark img json

--- 09/0913_extract_bookmark_img_json/main.py
+++ 09/0913_extract_bookmark_img_json/main.py
@@ -20,28 +20,29 @@ def make_logger(log):
 
 
 def readfiles(dir, Ext):
-    file_dict = defaultdict(str)
+    file_dict = defaultdict(list)
     for root, dirs, files in os.walk(dir):
         for file in files:
             filename, ext = os.path.splitext(file)
             if ext == Ext:
+                new_filename = filename.split('_')[0]
                 file_path = os.path.join(root, file)
                 
-                file_dict[filename] = file_path
+                file_dict[new_filename].append(file_path)
                 
     return file_dict
 
-def copy_file(input_path, ooutput_path):
+def copy_file(input_path, output_path):
     logger.info(input_path)
     root, file = os.path.split(input_path)
     mid = '\\'.join(root.split('\\')[len(root_dir.split('\\')):])
-    folder = os.path.join(ooutput_path, mid)
+    folder = os.path.join(output_path, mid)
     os.makedirs(folder, exist_ok=True)
     output_img_path = os.path.join(folder, file)
     shutil.copy2(input_path, output_img_path)
     logger.info(f'{output_img_path} 저장!!!')
     
-_, bookmark_dir, root_dir, otuput_dir, file_type = sys.argv
+_, bookmark_dir, root_dir, output_dir, file_type = sys.argv
 
 logger = make_logger('log.log')
 
@@ -50,23 +51,30 @@ json_dict = readfiles(root_dir, '.json')
 
 file_list = []
 
-with open(bookmark_dir, 'r', encoding='utf-16') as f:
-    bookmarks = f.readlines()
-    
-for bookmark in tqdm(bookmarks, desc='bookmark 읽는중'):
-    file = os.path.split(bookmark)[-1]
-    file = file.replace('\n', '')
-    filename, ext = os.path.splitext(file)
-    if ext == '.jpg' or ext == '.json':
-        file_list.append(filename)
+bookmark_list = os.listdir(bookmark_dir)
+
+for book in bookmark_list:
+    bookmark_path = os.path.join(bookmark_dir, book)
+
+    with open(bookmark_path, 'r', encoding='utf-16') as f:
+        bookmarks = f.readlines()
         
-if file_type == 'jpg':
-    for filename in tqdm(file_list, desc='파일복사중'):
-        img_path = img_dict.get(filename)
-        if img_path:
-            copy_file(img_path, otuput_dir)
-elif file_type == 'json':
-    for filename in tqdm(file_list, desc='파일복사중'):
-        json_path = json_dict.get(filename, otuput_dir)
-        if json_path:
-            copy_file(json_path)
+    for bookmark in tqdm(bookmarks, desc='bookmark 읽는중'):
+        file = os.path.split(bookmark)[-1]
+        file = file.replace('\n', '')
+        filename, ext = os.path.splitext(file)
+        if ext == '.jpg' or ext == '.json':
+            file_list.append(filename)
+            
+    if file_type == 'jpg':
+        for filename in tqdm(file_list, desc='파일복사중'):
+            img_paths = img_dict.get(filename)
+            if img_paths:
+                for img_path in img_paths:
+                    copy_file(img_path, output_dir)
+    elif file_type == 'json':
+        for filename in tqdm(file_list, desc='파일복사중'):
+            json_paths = json_dict.get(filename)
+            if json_paths:
+                for json_path in json_paths:
+                    copy_file(json_path, output_dir)
