39f4dbece5a2d46939b10d7d25221c521f3861f0
woojin sang <tkdalsrb147@naver.com>
2023-09-20 16:36:10 +0900

    revise json format

--- 09/0920_revise_json_format/main.py
+++ 09/0920_revise_json_format/main.py
@@ -1,5 +1,4 @@
 import os, sys, json
-from collections import defaultdict
 
 def readfiles(dir, Ext):
     file_list = []
@@ -12,13 +11,62 @@ def readfiles(dir, Ext):
                 file_list.append(file_path)
     return file_list
 
+json_format = {
+  "objects": [
+    {
+      "id": "",
+      "classId": "",
+      "name": "",
+      "type": "",
+      "valid": True,
+      "color": "",
+      "zOrder": 0,
+      "options": [],
+      "childrens": [],
+      "points": [
+        [
+          0,
+          0
+        ],
+        [
+          1080,
+          1920
+        ]
+      ],
+      "attributes": []
+    }
+  ],
+  "info": {
+    "imageName": "",
+    "width": None,
+    "height": None,
+    "labeler": "",
+    "examinator": "",
+    "timestamp": 0,
+    "format": "jpg",
+    "fileSize": 0,
+    "dirPath": "",
+    "projectName": "",
+    "taskName": ""
+  }
+}
+
+def makeOutputPath(file_path, file_dir, output_dir):
+    root, file = os.path.split(file_path)
+    file = file.replace('.jpg', '.json')
+    relpath = os.path.relpath(file_path, file_dir)
+    mid_dir = os.path.split(relpath)[0]
+    output_path = os.path.join(output_dir, mid_dir, file)
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    return output_path
+
 _, input_dir, output_dir = sys.argv
 
-json_list = readfiles(input_dir, '.json')
+jpg_list = readfiles(input_dir, '.jpg')
 
-for json_path in json_list:
-    
-    with open(json_path, encoding='utf-8') as f:
-        json_file = json.load(f)
-    
-    print(json_file)
+for jpg_path in jpg_list:
+    output_json_path = makeOutputPath(jpg_path, input_dir, output_dir)
+    print(output_json_path, '저장!')
+    with open(output_json_path, 'w', encoding='utf-8') as o:
+        json.dump(json_format, o, ensure_ascii=False, indent=2)
