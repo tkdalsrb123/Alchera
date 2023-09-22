4a444ac9be05612de7845005ef2272571a1eaba3
woojin sang <tkdalsrb147@naver.com>
2023-09-22 10:10:35 +0900

    create json

--- 09/0921_create_json_from_excel/main.py
+++ 09/0921_create_json_from_excel/main.py
@@ -20,10 +20,17 @@ def make_logger(log):
     
     return logger
 
-def makeJson(path, id, gender, age, result, qa):
+def makeJson(path, id, gender, age, language, logic, view, nature, music, individual, physical, review, q_a):
     json_tree = {}
     json_tree['object'] = [{"id":id, "gender":gender, "age":age}]
-    json_tree['annotation'] = {"text":{"result":result, "q_a":qa,}}
+    json_tree['annotation'] = {"text":{"result":{
+                                                "language":language, "logic":logic, "view":view, 
+                                                 "nature":nature, "music":music, "individual":individual, 
+                                                 "physical":physical, "review":review
+                                                 }, 
+                                       "q_a":q_a,
+                                       }
+                               }
     logger.info(f"{path}/{id}.json")
     with open(f"{path}/{id}.json", 'w', encoding='utf-8') as o:
         json.dump(json_tree, o, indent=2, ensure_ascii=False)
@@ -40,20 +47,33 @@ def replaceText(t):
             text = t.replace('세', '')
     return text
 
+def findNaN(n):
+    if pd.isnull(n) == True:
+        t = ""
+    elif type(n) == float:
+        t = str(int(n))
+    else:
+        t = n
+    return t
 
 def makeInfo(x, path):
     id = x.iloc[0]
     id = str(id).rjust(4, '0')
-    gender = replaceText(x.iloc[12])
-    age = replaceText(x.iloc[11])
-    result = ""
-    qa = ""
-    if type(x['result']) != float: 
-        result = x['result']
-    if type(x['q_a']) != float: 
-        qa = x['q_a']
+    gender = replaceText(x.iloc[1])
+    age = replaceText(x.iloc[2])
+    
+    language = findNaN(x.iloc[3])
+    logic = findNaN(x.iloc[4])
+    view = findNaN(x.iloc[5])
+    nature = findNaN(x.iloc[6])
+    music = findNaN(x.iloc[7])
+    individual = findNaN(x.iloc[8])
+    physical = findNaN(x.iloc[9])
+    review = findNaN(x.iloc[10])
+    q_a = findNaN(x.iloc[11])
+    
 
-    makeJson(path, id, gender, age, result, qa)
+    makeJson(path, id, gender, age, language, logic, view, nature, music, individual, physical, review, q_a)
     
 _, excel_dir, output_dir = sys.argv
 
