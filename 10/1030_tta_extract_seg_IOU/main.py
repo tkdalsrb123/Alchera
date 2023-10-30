7a54d2e5227096d0cc9cf10563701124185f5950
woojin sang <tkdalsrb147@naver.com>
2023-10-30 16:02:29 +0900

    tta extract seg iou

---
+++ 10/1030_tta_extract_seg_IOU/main.py
@@ -0,0 +1,75 @@
+import json, os, sys
+import logging
+from tqdm import tqdm
+from collections import defaultdict
+import pandas as pd
+from shapely.geometry import Polygon
+import numpy as np
+
+
+def make_logger(log):
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    
+    # formatter
+    file_formatter = logging.Formatter("%(asctime)s [%(levelname)s:%(lineno)d] -- %(message)s")
+    # file_handler
+    file_handler = logging.FileHandler(log, mode='w')
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.INFO)
+    # logger.add
+    logger.addHandler(file_handler)
+    
+    return logger
+    
+
+def readfiles(dir, Ext):
+    file_dict = defaultdict(list)
+
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            filename, ext = os.path.splitext(file)
+            ext = ext.lower()
+            if ext == Ext:
+                file_path = os.path.join(root, file)
+            
+                file_dict[filename] = file_path
+    return file_dict
+
+def openJson(path):
+    with open(path, encoding='utf-8-sig') as f:
+        json_file = json.load(f)
+    
+    return json_file
+
+if __name__ == "__main__":
+    _, source_dir, treed_dir, output_dir = sys.argv
+
+    logger = make_logger('log.log')
+    source_dict = readfiles(source_dir, '.json')
+    treed_dict = readfiles(treed_dir, '.json')
+
+    iou_list = []
+    for filename, source_path in tqdm(source_dict.items()):
+        logger.info(filename)
+        treed_path = treed_dict[filename]
+        
+        source_data = openJson(source_path)
+        treed_data = openJson(treed_path)
+        
+        if source_data['image_inside_info']['annotations']:
+            source_points = [[round(poly['x']), round(poly['y'])] for poly in source_data['image_inside_info']['annotations'][0]['polygon_info']]
+            treed_points = [[round(p[0]), round(p[1])] for p in treed_data['objects'][0]['points']]
+            try:
+                polygon1 = Polygon(source_points)
+                polygon2 = Polygon(treed_points)
+                intersect = polygon1.intersection(polygon2).area
+                union = polygon1.union(polygon2).area
+                iou_val = intersect/union
+                
+                iou_list.append([filename, iou_val])
+            except:
+                print(filename)
+                
+    df = pd.DataFrame(iou_list, columns=['filename', 'IoU'])
+    df.to_csv(f"{output_dir}/IoU_list.csv", index=False)
---
+++ 10/1030_tta_extract_seg_IOU/readme.txt
@@ -0,0 +1,7 @@
+1030 상우진
+
+1. 요청사항
+[[TTA] seg IoU 산출 코드 요청]
+
+2. 실행방법
+python main.py [source 경로] [treed 경로] [output 경로]
