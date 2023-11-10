db984c6899f3c9b7a48632b19b626eee6cc6f54d
woojin sang <tkdalsrb147@naver.com>
2023-11-10 13:31:35 +0900

    treeD bbox vis

--- 11/1109_treed_bbox_vis/main.py
+++ 11/1109_treed_bbox_vis/main.py
@@ -59,6 +59,49 @@ def readJson(path):
         data = json.load(f)
     return data
 
+def select_color(key):
+    color_mappings = {
+    "metal_knife": (192, 192, 192),
+    "metal_tongs": (128, 128, 128),
+    "metal_forks": (169, 169, 169),
+    "metal_spoon": (211, 211, 211),
+    "metal_chopsticks": (169, 169, 169),
+    "metal_ladle": (169, 169, 169),
+    "metal_grater": (169, 169, 169),
+    "eggs": (255, 255, 0),
+    "chestnut": (139, 69, 19),
+    "drumstick": (139, 69, 19),
+    "chicken_wing": (244, 164, 96),
+    "wedge_potato": (210, 105, 30),
+    "shrimp": (255, 165, 0),
+    "mackerel": (0, 0, 139),
+    "chocolate_chip_cookie": (210, 105, 30),
+    "madeleine": (244, 164, 96),
+    "squid": (0, 0, 0),
+    "abalone": (128, 128, 128),
+    "vienna_sausage": (244, 164, 96),
+    "triangle_rice_ball": (255, 255, 255),
+    "chicken_tender": (244, 164, 96),
+    "onion_ring": (139, 69, 19),
+    "potato_hotdog": (210, 105, 30),
+    "shrimp_toast": (255, 165, 0),
+    "chili_shrimp": (255, 69, 0),
+    "rolled_seaweed": (0, 128, 0),
+    "chicken_skewers": (255, 69, 0),
+    "buckweat_crepe": (139, 69, 19),
+    "something1": (255, 0, 0),
+    "something2": (0, 255, 0),
+    "something3": (0, 0, 255),
+    "something4": (255, 255, 0),
+    "something5": (0, 255, 255),
+    "something6": (255, 0, 255),
+    "something7": (128, 0, 0),
+    "something8": (0, 128, 0),
+    "something9": (0, 0, 128),
+    "something10": (128, 128, 0)
+    }
+    return color_mappings[key]
+
 _, img_dir, json_dir, output_dir = sys.argv
 
 logger = make_logger('log.log')
@@ -73,11 +116,12 @@ for filename, img_path in tqdm(img_dict.items()):
         
         json_file = readJson(json_path)
         
-        color = (0,0,255)
+        
         img = read_img(img_path)
         for obj in json_file['objects']:
             points = obj['points']
             name = obj['name']
+            color = select_color(name)
             x1y1 = tuple([round(p) for p in points[0]])
             x2y2 = tuple([round(p) for p in points[1]])
 
---
+++ 11/1109_treed_bbox_vis/readme.txt
@@ -0,0 +1,7 @@
+1110 상우진
+
+1. 요청사항
+treeD bbox vis
+
+2. 실행방법
+python main.py [image dir] [json dir] [output dir]
