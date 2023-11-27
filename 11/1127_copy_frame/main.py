6d5fb14290f1382591340659ca9dc6881e019889
woojin sang <tkdalsrb147@naver.com>
2023-11-27 14:15:23 +0900

    copy frame

---
+++ 11/1127_copy_frame/main.py
@@ -0,0 +1,36 @@
+import os, sys, shutil
+import pandas as pd
+from tqdm import tqdm
+from collections import defaultdict
+tqdm.pandas()
+
+def readfiles(dir, Ext):
+    file_dict = defaultdict(list)
+
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            filename, ext = os.path.splitext(file)
+            ext = ext.lower()
+            if ext == Ext:
+                file_path = os.path.join(root, file)
+            
+                file_dict[filename] = file_path
+    return file_dict
+
+def frame_copy(x):
+    no_frame_num = '_'.join(x['start'].split('_')[:5])
+    start_frame_num = int(os.path.splitext(x['start'])[0].split('_')[-1])
+    end_frame_num = int(os.path.splitext(x['end'])[0].split('_')[-1])
+    frame_list = [f"{no_frame_num}_{i.zfill(8)}" for i in range(start_frame_num, end_frame_num+1)]
+    for frame_name in frame_list:
+        jpg_path = jpg_dict.get(frame_name)
+        shutil.copy2(jpg_path, f'{output_dir}/{frame_name}.jpg')
+    
+if __name__ == "__main__":
+    _, input_dir, csv_dir, output_dir = sys.argv
+
+    jpg_dict = readfiles(input_dir, '.jpg')
+
+    df = pd.read_csv(csv_dir, encoding='utf-8')
+    df.progress_apply(frame_copy, axis=1)
+    
---
+++ 11/1127_copy_frame/readme.txt
@@ -0,0 +1,7 @@
+1127 상우진
+
+1. 요청사항
+[프레임 구간 복사]
+
+2. 실행방법
+python main.py [input dir] [csv dir] [output dir]
