dbf51f4819c2cb1e9cef663c9e66f53164dd3c80
woojin sang <tkdalsrb147@naver.com>
2023-12-04 16:27:36 +0900

    bbox vis

---
+++ 12/1204_tta_075-096_bbox_vis/main.py
@@ -0,0 +1,83 @@
+import os, sys, logging, json, cv2
+import numpy as np
+from tqdm import tqdm
+from collections import defaultdict
+
+def make_logger(log):
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    
+    # formatter
+    file_formatter = logging.Formatter("%(asctime)s [%(levelname)s:%(lineno)d] -- %(message)s")
+    # file_handler
+    file_handler = logging.FileHandler(log, mode='w')
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.INFO)
+    # logger.add
+    logger.addHandler(file_handler)
+    
+    return logger
+
+def readfiles(dir, Ext):
+    file_dict = defaultdict(list)
+
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            filename, ext = os.path.splitext(file)
+            ext = ext.lower()
+            if ext == Ext:
+                filename = filename[2:]
+                file_path = os.path.join(root, file)
+            
+                file_dict[filename] = file_path
+    return file_dict
+
+def makeOutputPath(file_path, file_dir, output_dir, Ext):
+    root, file = os.path.split(file_path)
+    filename, ext = os.path.splitext(file)
+    relpath = os.path.relpath(file_path, file_dir)
+    mid_dir = os.path.split(relpath)[0]
+    output_path = os.path.join(output_dir, mid_dir, f"{filename}.{Ext}")
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    return output_path
+
+def readJson(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+
+def read_img(img_path):
+    img_array = np.fromfile(img_path, np.uint8)
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    return img
+
+def save_img(img_path, img, ext):
+    result, encoded_img = cv2.imencode(f'.{ext}', img)
+    if result:
+        with open(img_path, mode='w+b') as f:
+            encoded_img.tofile(f)
+            
+if __name__ == "__main__":
+    _, input_dir, output_dir = sys.argv
+    
+    json_dict = readfiles(input_dir, '.json')
+    img_dict = readfiles(input_dir, '.jpeg')
+
+    for filename, json_path in tqdm(json_dict.items()):
+        img_path = img_dict[filename]
+        output_img_path = makeOutputPath(img_path, input_dir, output_dir, 'jpeg')
+        img = read_img(img_path)
+        data = readJson(json_path)
+
+        char_info = data['label']['character'].get('char_info')
+        if char_info:
+            for char in char_info:
+                x1 = round(char['bbox']['x'])
+                y1 = round(char['bbox']['y'])
+                x2 = x1 + round(char['bbox']['w'])
+                y2 = y1 + round(char['bbox']['h'])
+
+                cv2.rectangle(img, (x1,y1), (x2, y2), color=(0,0,255), thickness=3)
+        
+        save_img(output_img_path, img, 'jpeg')
---
+++ 12/1204_tta_075-096_bbox_vis/readme.txt
@@ -0,0 +1,7 @@
+1204 상우진
+
+1. 요청사항
+[[TTA] 075-096 바운딩 박스 시각화 코드 요청]
+
+2. 실행방법
+python main.py [input_dir] [output_dir]
