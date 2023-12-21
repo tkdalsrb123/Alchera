f2aecebc12b67300ad3046356fd2c606b131473e
woojin sang <tkdalsrb147@naver.com>
2023-12-21 17:15:28 +0900

    revise excel json

--- 12/1221_rsir_revise_excel_json/main.py
+++ 12/1221_rsir_revise_excel_json/main.py
@@ -46,8 +46,154 @@ def readJson(path):
         data = json.load(f)
     return data 
 
+def saveJson(file, path):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(file, f, indent=2, ensure_ascii=False)
+
+def preprocessing(data):
+    data_dict = {key:val for key, val in data.to_dict().items() if val != ""}
+    
+    info_list.append(data_dict)
+
+def json_format(data, quantity, name, city):
+    format = {
+        "Frame": quantity,
+        "Source": name,
+        "Time": "",
+        "Location": "",
+        "Weather": "",
+        "RoadType": "",
+        "RoadSurface": "",
+        "GPSCoordinate": "41.402588,41.402588",
+        "Place": city,
+        "TrafficCondition": "",
+        "IlluminationCondition": "",
+        "CameraCondition": "",
+        "TargetObjectMovement": "",
+        "Label": "3d_detection_RSIR_v3"
+    }
+    for key, value in data.items():
+        if value == 1:
+            val = mapping1[key]
+            if val != 'Normal':
+                format[mapping2[val]] = val
+            else:
+                if key == '일반.2':
+                    format["IlluminationCondition"] = val
+                elif key == '일반.3':
+                    format["CameraCondition"] = val
+                elif key == '차선변경':
+                    format["TargetObjectMovement"] = val
+                    
+    return format
+
+mapping1 = {
+    '주간': 'Day',
+    '야간': 'Night',
+    '고속화': 'D_Expressway',
+    '도심': 'D_Road',
+    '맑음': 'Sunny',
+    '흐림': 'Cloudy',
+    '안개': 'Fog',
+    '비': 'Rain',
+    '눈': 'Snow',
+    '일반.1': 'General',
+    'TG': 'TG',
+    'IC/JC': 'IC_JC',
+    '터널': 'Tunnel',
+    '교차로': 'Intersection',
+    '공사구간': 'RoadConstruction',
+    '아스팔트': 'Asphalt',
+    '콘크리트': 'Concrete',
+    '보도블럭': 'Pavement',
+    '흙': 'Dirt',
+    '자갈': 'Pebble',
+    '눈길': 'Icy',
+    '빗길': 'Waterly',
+    '원활': 'Light',
+    '혼잡': 'Congested',
+    '일반.2': 'Normal',
+    '태양역광': 'DirectSunlight',
+    '헤드램프': 'DirectHeadLight',
+    '가로등': 'DirectStreetLight',
+    '초저도도': 'VeryLowLight',
+    '일반.3': 'Normal',
+    '얼룩': 'Smear',
+    '물맺힘': 'WaterDrop',
+    '부분가림': 'PartialOcclusion',
+    '물체반사': 'ReflectionFromDashboard',
+    '차선유지': 'Cut-in',
+    '차선변경': 'Normal',
+    '기타': 'Others'
+}
+
+mapping2 = {
+    'Day': 'Time',
+    'Night': 'Time',
+    'D_Expressway': 'Location',
+    'D_Road': 'Location',
+    'Sunny': 'Weather',
+    'Cloudy': 'Weather',
+    'Fog': 'Weather',
+    'Snow': 'Weather',
+    'Rain': 'Weather',
+    'General': 'RoadType',
+    'TG': 'RoadType',
+    'IC_JC': 'RoadType',
+    'Tunnel': 'RoadType',
+    'Intersection': 'RoadType',
+    'RoadConstruction': 'RoadType',
+    'Asphalt': 'RoadSurface',
+    'Concrete': 'RoadSurface',
+    'Pavement': 'RoadSurface',
+    'Dirt': 'RoadSurface',
+    'Pebble': 'RoadSurface',
+    'Icy': 'RoadSurface',
+    'Waterly': 'RoadSurface',
+    'Light': 'TrafficCondition',
+    'Congested': 'TrafficCondition',
+    'Normal': ['IlluminationCondition', "CameraCondition", "TargetObjectMovement"],    
+    'DirectSunlight': 'IlluminationCondition',
+    'DirectHeadLight': 'IlluminationCondition',
+    'DirectStreetLight': 'IlluminationCondition',
+    'VeryLowLight': 'IlluminationCondition',
+    'Smear': 'CameraCondition',
+    'WaterDrop': 'CameraCondition',
+    'PartialOcclusion': 'CameraCondition',
+    'ReflectionFromDashboard': 'CameraCondition',
+    'Cut-in': 'TargetObjectMovement',
+    'Others': 'TargetObjectMovement'
+}
+
 if __name__ == "__main__":
     _, excel_dir, input_dir, output_dir = sys.argv
+    logger = make_logger('log.log')
+    info_list = []
+    df = pd.read_excel(excel_dir, na_filter=0)
+    df.apply(preprocessing, axis=1)
+    
+    for info in tqdm(info_list):
+        input_dirs = os.path.join(input_dir, info['시나리오'], info['시퀀스'], f"RR_SD_CMR_{info['위치']}")
+        
+        frame_list = []
+        logger.info(input_dirs)
+        try:
+            for file in os.listdir(input_dirs):
+                
+                if 'Property' in file:
+                    json_path = os.path.join(input_dirs, file)
+                    output_json_path = makeOutputPath(json_path, input_dir, output_dir, 'json')
+                elif 'Frame' in file:
+                    frame_dir = os.path.join(input_dirs, file)
+                    frame_list = os.listdir(frame_dir)
+        except FileNotFoundError:
+            print(input_dirs, '경로가 없습니다!!')
+            
+        if frame_list:
+            json_data_list = []
+            frame_list = [f"{os.path.splitext(frame)[0]}.jpg" for frame in frame_list if os.path.splitext(frame)[-1] == '.png']
+            for frame in frame_list:
+                json_data_list.append(json_format(info, info['수량'], frame, info['도시']))
+        
 
-    df = pd.read_excel(excel_dir)
-    print(df.loc[1])
+            saveJson(json_data_list, output_json_path)
